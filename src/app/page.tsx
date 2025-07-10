8382bde0731e3519cd5b3836dc4cf35a14cfc705
keiichirotakahashi <k16takahashi125@gmail.com>
2025-07-10 17:38:59 +0900

    feat: add solution section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Features02Page from "@/components/features-02/features-02";
+import Features06Page from "@/components/features-06/features-06";
 import Hero02 from "@/components/hero-02/hero-02";
 import Navbar04Page from "@/components/navbar-04/navbar-04";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar04Page />
       <Hero02 />
       <Features02Page />
+      <Features06Page />
     </div>
   );
 }
