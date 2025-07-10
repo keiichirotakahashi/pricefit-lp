f3072f237ac5bb6359a38fccf3c14c28735917d4
keiichirotakahashi <k16takahashi125@gmail.com>
2025-07-10 17:42:26 +0900

    feat: add flow section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Features02Page from "@/components/features-02/features-02";
 import Features06Page from "@/components/features-06/features-06";
 import Hero02 from "@/components/hero-02/hero-02";
 import Navbar04Page from "@/components/navbar-04/navbar-04";
+import Timeline05 from "@/components/timeline-05/timeline-05";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero02 />
       <Features02Page />
       <Features06Page />
+      <Timeline05 />
     </div>
   );
 }
