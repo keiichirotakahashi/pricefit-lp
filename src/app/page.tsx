a9477dc5780a986b3dab8585ebebde273915e8ee
keiichirotakahashi <k16takahashi125@gmail.com>
2025-07-10 18:13:58 +0900

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Contact02Page from "@/components/contact-02/contact-02";
 import FAQ07 from "@/components/faq-07/faq-07";
 import Features02Page from "@/components/features-02/features-02";
 import Features06Page from "@/components/features-06/features-06";
+import Footer05Page from "@/components/footer-05/footer-05";
 import Hero02 from "@/components/hero-02/hero-02";
 import Navbar04Page from "@/components/navbar-04/navbar-04";
 import Timeline05 from "@/components/timeline-05/timeline-05";
@@ -16,6 +17,7 @@ export default function Home() {
       <Timeline05 />
       <FAQ07 />
       <Contact02Page />
+      <Footer05Page />
     </div>
   );
 }
