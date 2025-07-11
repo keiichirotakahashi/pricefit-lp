599f0b530c7c4197a1864575234f0a73b6677ba9
keiichirotakahashi <k16takahashi125@gmail.com>
2025-07-11 18:34:49 +0900

    feat: insert contents in Hero section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpRight, CirclePlay } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
@@ -8,21 +8,17 @@ const Hero = () => {
       <div className="max-w-screen-xl w-full mx-auto grid lg:grid-cols-2 gap-12 px-6 py-12">
         <div>
           <Badge className="bg-gradient-to-br via-70% from-primary via-muted/30 to-primary rounded-full py-1 border-none">
-            Just released v1.0.0
+            β版提供中
           </Badge>
           <h1 className="mt-6 max-w-[17ch] text-4xl md:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold !leading-[1.2]">
-            Customized Shadcn UI Blocks & Components
+            メディアモニタリングをスマートに
           </h1>
           <p className="mt-6 max-w-[60ch] text-lg">
-            Explore a collection of Shadcn UI blocks and components, ready to preview and copy.
-            Streamline your development workflow with easy-to-implement examples.
+            ClipBaseは小規模な広報チームのためのメディアモニタリングツールです。リアルタイム通知&自動クリッピングで、反響も感情もすぐに見える化します。
           </p>
           <div className="mt-12 flex items-center gap-4">
             <Button size="lg" className="rounded-full text-base">
-              Get Started <ArrowUpRight className="!h-5 !w-5" />
-            </Button>
-            <Button variant="outline" size="lg" className="rounded-full text-base shadow-none">
-              <CirclePlay className="!h-5 !w-5" /> Watch Demo
+              無料トライアルに申し込む <ArrowUpRight className="!h-5 !w-5" />
             </Button>
           </div>
         </div>
