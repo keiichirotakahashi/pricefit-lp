aa50b04fc960197f5eb594932b47cf9ac571c0bd
keiichirotakahashi <k16takahashi125@gmail.com>
2025-08-02 22:57:32 +0900

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import { GA_ID } from "@/constants/ga";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +21,8 @@ const notoSansJP = Noto_Sans_JP({
 });
 
 export const metadata: Metadata = {
-  title: "ClipBase",
-  description: "ClipBaseのランディングページ",
+  title: "Webクリッピングツール「ClipBase（クリップベース）」",
+  description: "ClipBaseは業界最安水準で利用できるWebクリッピングツールです。広報・PR業務の自動化や効果測定を、おトクに、カンタンに始められます。",
 };
 
 export default function RootLayout({
@@ -34,6 +36,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${notoSansJP.variable} antialiased`}
       >
         {children}
+        {GA_ID && (
+          <GoogleAnalytics gaId={GA_ID} />
+        )}
       </body>
     </html>
   );
